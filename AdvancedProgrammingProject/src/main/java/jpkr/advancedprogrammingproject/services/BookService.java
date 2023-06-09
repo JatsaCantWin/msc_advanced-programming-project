d1d0bd9486d48f9ba1d37bbc2e9c90ac4b6a3d11
Piotr Jurek <piotrjurek9807@gmail.com>
2023-06-09 15:01:43 +0200

    feat: /books endpoint implemented (#2)

---
+++ AdvancedProgrammingProject/src/main/java/jpkr/advancedprogrammingproject/services/BookService.java
@@ -0,0 +1,54 @@
+package jpkr.advancedprogrammingproject.services;
+
+import jpkr.advancedprogrammingproject.models.Book;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class BookService {
+    private final MongoTemplate mongoTemplate;
+
+    public BookService(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    public Book saveBook(Book book) {
+        return mongoTemplate.save(book);
+    }
+
+    public List<Book> getAllBooks() {
+        return mongoTemplate.findAll(Book.class);
+    }
+
+    public List<Book> getFilteredBooks(String author, Integer releaseYear, String title) {
+        List<Criteria> criteriaList = new ArrayList<>();
+
+        if (author != null) {
+            criteriaList.add(Criteria.where("author").regex(author, "i"));
+        }
+        if (releaseYear != null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(releaseYear, 0, 1, 0, 0, 0);
+            Date startDate = calendar.getTime();
+            calendar.set(releaseYear, 11, 31, 23, 59, 59);
+            Date endDate = calendar.getTime();
+
+            criteriaList.add(Criteria.where("releaseDate").gte(startDate).lte(endDate));
+        }
+        if (title != null) {
+            criteriaList.add(Criteria.where("title").regex(title, "i"));
+        }
+
+        Criteria criteria = new Criteria().andOperator(criteriaList.toArray(new Criteria[0]));
+        Query query = new Query(criteria);
+
+        return mongoTemplate.find(query, Book.class);
+    }
+}
