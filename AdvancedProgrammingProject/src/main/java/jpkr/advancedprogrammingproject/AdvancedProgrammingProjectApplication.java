f1b5d5a2535a320fd4f7ac0944d979c4b27513f5
Piotr Jurek <piotrjurek9807@gmail.com>
2023-06-09 13:07:32 +0200

    chore: initalized Spring WebFlux project added Spring Security and Spring MongoDB

---
+++ AdvancedProgrammingProject/src/main/java/jpkr/advancedprogrammingproject/AdvancedProgrammingProjectApplication.java
@@ -0,0 +1,13 @@
+package jpkr.advancedprogrammingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedProgrammingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedProgrammingProjectApplication.class, args);
+    }
+
+}
---
+++ AdvancedProgrammingProject/src/test/java/jpkr/advancedprogrammingproject/AdvancedProgrammingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jpkr.advancedprogrammingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedProgrammingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
