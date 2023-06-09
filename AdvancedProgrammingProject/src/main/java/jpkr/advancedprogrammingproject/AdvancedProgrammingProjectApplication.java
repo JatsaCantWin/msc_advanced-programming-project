426e98fb606ec9dab1944d932a9a2140cad0d37b
Piotr Jurek <piotrjurek9807@gmail.com>
2023-06-09 14:59:41 +0200

    feat: simple testing Spring Security configuration allows only the /books endpoint and denies the rest

--- AdvancedProgrammingProject/src/main/java/jpkr/advancedprogrammingproject/AdvancedProgrammingProjectApplication.java
+++ AdvancedProgrammingProject/src/main/java/jpkr/advancedprogrammingproject/AdvancedProgrammingProjectApplication.java
@@ -2,6 +2,11 @@ package jpkr.advancedprogrammingproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @SpringBootApplication
 public class AdvancedProgrammingProjectApplication {
@@ -10,4 +15,13 @@ public class AdvancedProgrammingProjectApplication {
         SpringApplication.run(AdvancedProgrammingProjectApplication.class, args);
     }
 
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange((authorize) -> authorize
+                        .pathMatchers("/books").permitAll()
+                        .anyExchange().denyAll()
+                );
+        return http.build();
+    }
 }
