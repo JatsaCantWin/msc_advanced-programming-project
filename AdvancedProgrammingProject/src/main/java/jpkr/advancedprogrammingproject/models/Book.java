c21035a5a86f9520dc134c8616c3987fda2760c9
Piotr Jurek <piotrjurek9807@gmail.com>
2023-06-09 14:51:18 +0200

    feat: book model

---
+++ AdvancedProgrammingProject/src/main/java/jpkr/advancedprogrammingproject/models/Book.java
@@ -0,0 +1,58 @@
+package jpkr.advancedprogrammingproject.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.util.Date;
+
+@Document(collection = "books")
+public class Book {
+    @Id
+    private String id;
+    private String title;
+    private String author;
+    @Field(targetType = FieldType.DATE_TIME)
+    private Date releaseDate;
+    private String ISBN;
+
+    public Book(String title, String author, Date releaseDate, String ISBN) {
+        this.title = title;
+        this.author = author;
+        this.releaseDate = releaseDate;
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+}
